f425ba419f18bd66b7546c786c843da83b468d0d
Daniel <142172231+danielquirozc@users.noreply.github.com>
2024-06-29 20:33:05 -0400

    Change the path of the 'ratelimit.js' file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { corsMiddleware } from "./middlewares/cors.js";
 import { gamesRouter } from "./routes/games.js";
 import express, { json } from "express";
 import dotenv from "dotenv";
-import { limiter } from "./ratelimit.js";
+import { limiter } from "./middlewares/ratelimit.js";
 dotenv.config();
 
 // Server
