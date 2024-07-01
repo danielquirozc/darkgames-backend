0083b2aa9a94986ba893939383e43ce33d3466bb
Daniel <142172231+danielquirozc@users.noreply.github.com>
2024-07-01 16:21:58 -0400

    Change rate-limit configuration

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -3,5 +3,7 @@ import { rateLimit } from 'express-rate-limit'
 export const limiter = rateLimit({
 	windowMs: 5 * 60 * 1000, 
 	limit: 50, 
-	legacyHeaders: false, 
+	legacyHeaders: true,
+	validate: { xForwardedForHeader: false },
+	standardHeaders: true 
 })
